26e979b1b869528443b22c01688cd2f22f31cec8
David Gwynne <dlg@uq.edu.au>
2023-07-25 11:46:19 +1000

    Import OPENBSD_7_3 branch

---
+++ usr.sbin/bgpd/version.h
@@ -0,0 +1,3 @@
+/* $OpenBSD: version.h,v 1.10 2023/03/22 17:14:57 claudio Exp $ */
+
+#define BGPD_VERSION	"7.9"
---
+++ usr.sbin/rpki-client/version.h
@@ -0,0 +1,3 @@
+/* $OpenBSD: version.h,v 1.15 2023/03/14 13:31:16 claudio Exp $ */
+
+#define RPKI_VERSION	"8.3"
