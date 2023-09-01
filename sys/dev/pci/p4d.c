f6b5872fcac7739261e4b6d97b6cbf490c9a7794
s4529458 <kait.lam@uq.edu.au>
2023-09-01 11:41:03 +1000

    bus_space_vaddr.

--- sys/dev/pci/p4d.c
+++ sys/dev/pci/p4d.c
@@ -42,29 +42,45 @@ p4d_match(struct device *parent, void *match, void *aux)
 	return (0);
 }
 
+struct p4d_bar {
+        uint64_t        a;
+        uint64_t        b;
+        uint64_t        sum;
+};
+
 static void
 p4d_attach(struct device *parent, struct device *self, void *aux)
 {
 	printf(": hello world x2 x2\n");
 	struct pci_attach_args *paa = aux;
-	struct p4d_softc *softc = (struct p4d_softc*)self;
+	struct p4d_softc *sc = (struct p4d_softc*)self;
 	pcireg_t reg_type = pci_mapreg_type(paa->pa_pc, paa->pa_tag, 0x10);
 	bus_size_t size = 0;
 
 	pci_mapreg_map(paa,
 			0x10,
 			reg_type,
-			0, 
-			&softc->tag, 
-			&softc->handle,
+			BUS_SPACE_MAP_LINEAR, 
+			&sc->tag, 
+			&sc->handle,
 			NULL, 
 			&size,
 			0);
 	printf(": size %lx\n", size);
 	printf(": hello done :3\n");
 
-	bus_space_write_8(softc->tag, softc->handle, 0x00, 42);
-	bus_space_write_8(softc->tag, softc->handle, 0x08, 8);
-	unsigned long long v = bus_space_read_8(softc->tag, softc->handle, 0x10);
+	bus_space_write_8(sc->tag, sc->handle, 0x00, 42);
+	bus_space_write_8(sc->tag, sc->handle, 0x08, 8);
+	unsigned long long v = bus_space_read_8(sc->tag, sc->handle, 0x10);
 	printf("p4d read sum = %llu\n", v);
+
+	struct p4d_bar* bar;
+	bar = bus_space_vaddr(sc->tag, sc->handle);
+	bus_space_barrier(sc->tag, sc->handle, 0, size,
+            BUS_SPACE_BARRIER_WRITE);
+	bar->a = 42;
+        bar->b = 8;
+        bus_space_barrier(sc->tag, sc->handle, 0, size,
+            BUS_SPACE_BARRIER_WRITE | BUS_SPACE_BARRIER_READ);
+        printf("p6d read sum = %llu\n", bar->sum);
 }
