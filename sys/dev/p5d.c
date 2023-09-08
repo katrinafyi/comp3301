ad53e6b7367bf1aefd366c53250a1f6dc3d9de5d
s4529458 <kait.lam@uq.edu.au>
2023-09-08 11:25:33 +1000

    restore missing open etc.

--- sys/dev/p5d.c
+++ sys/dev/p5d.c
@@ -28,6 +28,25 @@ p5dattach(int n)
 }
 
 int
+p5dopen(dev_t dev, int mode, int flags, struct proc *p)
+{
+	printf("hello p5d world\n");
+	return (0);
+}
+
+int
+p5dclose(dev_t dev, int flag, int mode, struct proc *p)
+{
+	return (0);
+}
+
+int
+p5dioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
+{
+	return (ENXIO);
+}
+
+int
 p5dwrite(dev_t dev, struct uio *uio, int flags)
 {
 	int eno;
